fab0efe253fe19a824b6161b8334e9398f3abe00
Heat Koemnak <heatkimnak@gmail.com>
2026-03-18 17:41:31 +0700

    update teaceibility

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,16 +1,24 @@
 
 import clsx from "clsx";
+import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
-import { FaRegCircleCheck } from "react-icons/fa6";
 
-export const Card = ({ title, description }) => {
+export const Card = ({ title, description, link }) => {
+  const isExternal = typeof link === "string" && /^(https?:)?\/\//.test(link);
+  const Wrapper = link ? Link : "div";
+  const wrapperProps = link
+    ? {
+        className: clsx(styles.card, styles.card_compact, styles.card_clickable),
+        ...(isExternal
+          ? { href: link, target: "_blank", rel: "noopener noreferrer" }
+          : { to: link }),
+      }
+    : { className: clsx(styles.card, styles.card_compact) };
+
   return (
-    <div className={clsx(styles.card, styles.card_compact)}>
-      {/* <div className={clsx(styles.card_icon_wrap, styles.card_icon_wrap_compact)} aria-hidden="true">
-        <FaRegCircleCheck className={clsx(styles.card_icon, styles.card_icon_compact)} />
-      </div> */}
+    <Wrapper {...wrapperProps}>
       <span className={clsx(styles.card_title, styles.card_title_compact)}>{title}</span>
       <p className={clsx(styles.card_description, styles.card_description_compact)}>{description}</p>
-    </div>
+    </Wrapper>
   );
 };
