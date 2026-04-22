0a76e1e62250be9381b77e0fb9b61e905d5f18a2
Heat Koemnak <heatkimnak@gmail.com>
2026-04-22 17:03:34 +0700

    feat: add getting-started and sales pages and generate image usage reports

--- src/pages/getting-started/index.js
+++ src/pages/getting-started/index.js
@@ -1,26 +1,12 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import DefaultLayout from "../../layout/DefaultLayout";
-
-import { LayoutFeatures } from "../../components/LayoutFeatures";
 import MainLayout from "../../components/MainLayout";
 import DynamicPage from "../dynamic";
-const pageData = require("../../../config/gettstarted/index.json");
 export default function GetingStarted() {
-  const { siteConfig } = useDocusaurusContext();
 
   return (
-    // <Layout
-    //   title={pageData.title || siteConfig.title}
-    //   description={pageData.description || siteConfig.tagline}
-    // >
-    //    {pageData && pageData.blocks ? <LayoutFeatures route={pageData.route} blocks={pageData.blocks} /> : null}
-    // </Layout>
     <DefaultLayout>
       <MainLayout>
         <DynamicPage slug="getting-started" />
-       {/* {pageData && pageData.blocks ? <LayoutFeatures route={pageData.route} blocks={pageData.blocks} /> : null} */}
       </MainLayout>
     </DefaultLayout>
   );
